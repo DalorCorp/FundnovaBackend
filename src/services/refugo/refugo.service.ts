2ed1b889cc4d76f430e61fb035c3389ce5587416
Vinicius Fortunato <vinicius.fortunato.andrade@gmail.com>
2025-06-20 11:30:48 -0300

    20.06.2025

--- src/services/refugo/refugo.service.ts
+++ src/services/refugo/refugo.service.ts
@@ -147,7 +147,6 @@ export default class RefugoServices {
       const merged = {refugoKg, refugoQt}
       console.log("refugoKg");
       console.log("refugoKg");
-      console.log("refugoKg");
       
     return {
       type: null,
