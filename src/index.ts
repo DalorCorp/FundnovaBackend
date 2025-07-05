b222a2bb0be45f06cab4f0d92625d388c08863a9
DalorCorp <dalor.corporation@gmail.com>
2025-07-05 07:14:07 -0300

    5.7.2025

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import FaturamentoRouter from "./routers/faturamento/faturamento.router";
 import RefugoRouter from "./routers/refugo/refugo.router";
 import fs from "fs";
 import https from "https";
+import path from "path";
 
 export default class App {
   public app: express.Express;
@@ -34,10 +35,11 @@ export default class App {
   }
 
   public start(PORT: string | number):void {
+    const certsPath = path.resolve(__dirname, "../Certs");
     const options = {
-      key: fs.readFileSync("../Certs/fundnovacloud.origus.com.br-key.pem"),
-      cert: fs.readFileSync("../Certs/fundnovacloud.origus.com.br-crt.pem"),
-      ca: fs.readFileSync("../Certs/fundnovacloud.origus.com.br-chain.pem"),
+      key: fs.readFileSync(path.join(certsPath, "fundnovacloud.origus.com.br-key.pem")),
+      cert: fs.readFileSync(path.join(certsPath, "fundnovacloud.origus.com.br-crt.pem")),
+      ca: fs.readFileSync(path.join(certsPath, "fundnovacloud.origus.com.br-chain.pem")),
     };
     https.createServer(options, this.app).listen(Number(PORT), '0.0.0.0', () => {
       console.log(`Servidor HTTPS rodando na porta ${PORT}`);
