51feadfec989dd2d8cf32b927e22cacf6faecf98
DalorCorp <dalor.corporation@gmail.com>
2025-07-06 05:42:47 -0300

    6/7/2025

--- src/index.ts
+++ src/index.ts
@@ -6,25 +6,30 @@ import RefugoRouter from "./routers/refugo/refugo.router";
 
 export default class App {
   public app: express.Express;
-
   constructor () {
-    this.app = express();
+    this.app = express ();
     this.config();
-
     this.app.use("/pagamento", PagamentoRouter);
     this.app.use("/faturamento", FaturamentoRouter);
     this.app.use("/refugo", RefugoRouter);
   }
-
-  private config(): void {
+  private config():void {
     this.app.use(cors());
-
+    
+    // const accessControl: express.RequestHandler = (_req, res, next) => {
+    //   res.header("Access-Control-Allow-Origin", "*");
+    //   res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS,PUT,PATCH");
+    //   res.header("Access-Control-Allow-Headers", "*");
+    //   next();
+    // };
+    // this.app.use(accessControl);
     this.app.use(express.json());
+    
   }
 
-  public start(PORT: string | number): void {
+  public start(PORT: string | number):void {
     this.app.listen(Number(PORT), '0.0.0.0', () => {
-      console.log(`Servidor HTTP rodando na porta ${PORT}`);
+      console.log(`Running on port ${PORT}`);
     });
   }
 }
