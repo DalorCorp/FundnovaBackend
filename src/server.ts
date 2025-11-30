15dd6e030c55bd5a798d485ee61c1fefbde11f5b
DalorCorp <dalor.corporation@gmail.com>
2025-11-30 20:39:33 -0300

    30-11-2025

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,5 @@
 import "dotenv/config";
 import App from "./index";
-import http from "http";
 
-const PORT = process.env.APP_PORT || 30001; // HTTPS port
-const HTTP_PORT = 80; // HTTP port
-
-// Start HTTPS server
+const PORT = 3001;
 new App().start(PORT);
-
-// Optional: Redirect HTTP → HTTPS
-http.createServer((req, res) => {
-  const host = req.headers.host?.replace(/:\d+$/, `:${PORT}`);
-  res.writeHead(301, { Location: `https://${host}${req.url}` });
-  res.end();
-}).listen(HTTP_PORT, () => {
-  console.log(`Redirecionando HTTP (porta ${HTTP_PORT}) para HTTPS (porta ${PORT})`);
-});
