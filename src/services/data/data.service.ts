ab9bda761d88b5a225a455024bfd2047a3d156ea
unknown <dalor.corporation@gmail.com>
2026-01-11 20:30:55 -0300

    2026.01.11

--- src/services/data/data.service.ts
+++ src/services/data/data.service.ts
@@ -16,22 +16,22 @@ export default class DataServices {
   ];
 
 
-  private monthAbbrevToNumber(month: string): number | null {
+  private monthAbbrevToNumber(month: string): string | null {
     if (!month) return null;
   
-    const map: Record<string, number> = {
-      JAN: 1,
-      FEV: 2,
-      MAR: 3,
-      ABR: 4,
-      MAI: 5,
-      JUN: 6,
-      JUL: 7,
-      AGO: 8,
-      SET: 9,
-      OUT: 10,
-      NOV: 11,
-      DEZ: 12
+    const map: Record<string, string> = {
+      JAN: "Janeiro",
+      FEV: "Fevereiro",
+      MAR: "Março",
+      ABR: "Abril",
+      MAI: "Maio",
+      JUN: "Junho",
+      JUL: "Julho",
+      AGO: "Agosto",
+      SET: "Setembro",
+      OUT: "Outubro",
+      NOV: "Novembro",
+      DEZ: "Dezembro"
     };
   
     const key = month
